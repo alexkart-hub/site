1223d6104183e9f07ac42a97b3792284a6a9a1f0
A.Kartavtsev <a.kartavtsev@mediasoft.team>
2022-11-04 01:53:53 +0300

    SITE-2. Убрал кнопку из меню. Добавил пункты в меню. Добавил надпись

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -33,7 +33,7 @@
 <!--[if lte IE 9]>
 <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
 <![endif]-->
-<!-- header-start -->
+
 <header>
     <div class="header-area">
         <div id="sticky-header" class="main-header-area">
@@ -53,20 +53,8 @@
                                     <ul id="navigation">
                                         <li><a href="{{ route('home') }}">Главная</a></li>
                                         <li><a href="{{ route('themes') }}">Темы</a></li>
-                                        <li><a href="#">pages <i class="ti-angle-down"></i></a>
-                                            <ul class="submenu">
-                                                <li><a href="candidate.html">Candidates </a></li>
-                                                <li><a href="job_details.html">job details </a></li>
-                                                <li><a href="elements.html">elements</a></li>
-                                            </ul>
-                                        </li>
-                                        <li><a href="#">blog <i class="ti-angle-down"></i></a>
-                                            <ul class="submenu">
-                                                <li><a href="blog.html">blog</a></li>
-                                                <li><a href="single-blog.html">single-blog</a></li>
-                                            </ul>
-                                        </li>
-                                        <li><a href="contact.html">Contact</a></li>
+                                        <li><a href="#">Избранное</a></li>
+                                        <li><a href="#">Контакты</a></li>
                                     </ul>
                                 </nav>
                             </div>
@@ -74,10 +62,7 @@
                         <div class="col-xl-3 col-lg-3 d-none d-lg-block">
                             <div class="Appointment">
                                 <div class="phone_num d-none d-xl-block">
-                                    <a href="#">Log in</a>
-                                </div>
-                                <div class="d-none d-lg-block">
-                                    <a class="boxed-btn3" href="#">Post a Job</a>
+                                    <a href="#">Войти</a>
                                 </div>
                             </div>
                         </div>
@@ -86,7 +71,6 @@
                         </div>
                     </div>
                 </div>
-
             </div>
         </div>
     </div>
--- resources/views/themes/item.blade.php
+++ resources/views/themes/item.blade.php
@@ -1,6 +1,6 @@
 <div class="col-lg-4 col-xl-3 col-md-6">
     <div class="single_catagory">
         <a href="{{ route('theme', $theme->code) }}"><h2>{{ $theme->title }}</h2></a>
-        <p> <span>50</span> Available position</p>
+        <p>Кол-во записей <span>50</span></p>
     </div>
 </div>
