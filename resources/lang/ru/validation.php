0b60f97befc24c05f35af6bb0b3a91a68a0652e3
A.Kartavtsev <a.kartavtsev@mediasoft.team>
2022-11-12 23:49:11 +0300

    121122. Русифицировал некоторые сообщения валидатора

--- resources/lang/ru/validation.php
+++ resources/lang/ru/validation.php
@@ -1,4 +1,11 @@
 <?php
 return [
     'required' => 'Поле :attribute является обязательным.',
+    'min' => [
+        'numeric' => 'Значение :attribute должно быть не менее :min.',
+        'file' => 'Размер :attribute должно быть не менее :min килобайт.',
+        'string' => 'Поле :attribute должно быть не менее :min символов.',
+        'array' => 'Список :attribute должен содержать не менее :min элементов.',
+    ],
+
 ];
